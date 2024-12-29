08e7fa116758e46c11f2e45ea9fa767f3f65bd74
Pavel Grigoryev <pagri.analytics@gmail.com>
2024-12-29 12:38:16 +0300

    [main] at 2024-12-29 12:38:15 with changes: dash_apps/flask_app/dash_app0.py dash_apps/nginx/nginx.conf dash_apps/yandex_afisha_app.zip quarto/projects/tt/dashboard_for_yandex_afisha.ipynb quarto/projects/tt/dashboard_for_yandex_afisha_app.py quarto/projects/tt/dashboard_scheme.png quarto/projects/tt/temp.ipynb prev commit: 7bf22f8

--- dash_apps/flask_app/dash_app0.py
+++ dash_apps/flask_app/dash_app0.py
@@ -7,19 +7,13 @@ def init_dash(server):
     dash_app1 = dash.Dash(__name__, server=server, url_base_pathname='/api/dash0/')
 
     dash_app1.layout = html.Div([
-        html.H1('Dash App1'),
-        dcc.Graph(id='graph1'),
-        dcc.Interval(id='interval1', interval=1000)
+        html.H1(' ', id='title1'),
+        dcc.Interval(id='interval1', interval=60000)
     ])
 
     @dash_app1.callback(
-        Output('graph1', 'figure'),
+        Output('title1', 'children'),
         [Input('interval1', 'n_intervals')]
     )
     def update_graph1(n):
-        df = pd.DataFrame({
-            'x': [1, 2, 3],
-            'y': [1, 2, 3]
-        })
-        fig = px.line(df, x='x', y='y')
-        return fig
+        return ' '
